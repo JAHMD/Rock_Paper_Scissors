3a2e50235acb7eebfddefc320b5b474195e1efc7
Ahmed <ahmed@em.com>
2023-01-26 12:22:27 +0200

    select you play feature

--- public/index.js
+++ public/index.js
@@ -1,10 +1,36 @@
+const gameContainer = document.getElementById("game-container");
+const gameOptins = document.querySelectorAll(".game-item");
+// result
+const resultSection = document.getElementById("result-section");
+const selectedItemHolder = document.querySelectorAll(".selected-item-holder");
+// rules
 const rulesSection = document.getElementById("rules-section");
 const rulesBtn = document.getElementById("rules-btn");
 const closeRulesBtn = document.getElementById("close-rules-btn");
 
+// open rules section
 rulesBtn.addEventListener("click", rulesSectionHandler);
+// close rules section
 closeRulesBtn.addEventListener("click", rulesSectionHandler);
+// start the game
+gameOptins.forEach((option) => {
+  option.addEventListener("click", () => {
+    gameContainer.style.display = "none";
+    resultSection.classList.add("active");
+    resultSectionHandler.bind(option, option)();
+  });
+});
 
+// functions secton
 function rulesSectionHandler() {
   rulesSection.classList.toggle("active");
+}
+
+function resultSectionHandler(selectedItem) {
+  selectedItemHolder[0].innerHTML += `
+    <div class="result-item item absolute shadow-primary-${selectedItem.dataset.item}To/60">
+      <span class="icon-holder border-primary-${selectedItem.dataset.item}To">
+        <img src="./images/icon-${selectedItem.dataset.item}.svg" alt="${selectedItem.dataset.item} icon" />
+      </span>
+    </div>`;
 }
