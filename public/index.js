14bc2c92b2ba87eda1aefb23009b23a56d172b34
Ahmed <ahmed@em.com>
2023-01-26 12:03:14 +0200

    select a play feature

--- public/index.js
+++ public/index.js
@@ -1,10 +1,34 @@
+const gameContainer = document.getElementById("game-container");
+const gameOptins = document.querySelectorAll(".game-item");
+// result
+const resultSection = document.getElementById("result-section");
+const selectedItemHolder = document.querySelectorAll(".selected-item-holder");
+// rules
 const rulesSection = document.getElementById("rules-section");
 const rulesBtn = document.getElementById("rules-btn");
 const closeRulesBtn = document.getElementById("close-rules-btn");
 
+// open rules section
 rulesBtn.addEventListener("click", rulesSectionHandler);
+// close rules section
 closeRulesBtn.addEventListener("click", rulesSectionHandler);
+// start the game
+gameOptins.forEach((option) => {
+  option.addEventListener("click", resultSectionHandler.bind(option, option));
+});
 
+// functions secton
 function rulesSectionHandler() {
   rulesSection.classList.toggle("active");
+}
+
+function resultSectionHandler(selectedItem) {
+  selectedItemHolder[0].innerHTML += `
+    <div class="result-item item absolute shadow-primary-${selectedItem.dataset.item}To/60">
+      <span class="icon-holder border-primary-${selectedItem.dataset.item}To">
+        <img src="./images/icon-${selectedItem.dataset.item}.svg" alt="${selectedItem.dataset.item} icon" />
+      </span>
+    </div>`;
+  gameContainer.style.display = "none";
+  resultSection.classList.add("active");
 }
